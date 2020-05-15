df414b521716dd9d8806ee8dfefecfbc27331b5c
steve <stevemenezes70@gmail.com>
2020-05-15 19:50:23 -0400

    Geolocation added to DataComp

--- src/DataComp.js
+++ src/DataComp.js
@@ -1,12 +1,13 @@
 import InputSearch from './InputSearch';
 import React from 'react';
-
+import Geolocation from './Gelocation';
 class DataComp extends React.Component
 {
   render(){
     return (
       <div>
         <InputSearch />
+        <Geolocation />
       </div>
 
     )
