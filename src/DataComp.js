ee9f6b2c8e3e0dbc9223a2dc097cb8916edd817e
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-05-16 01:14:37 -0400

    Data Comp Changes

--- src/DataComp.js
+++ src/DataComp.js
@@ -1,24 +1,36 @@
 import InputSearch from './InputSearch';
 import React from 'react';
 import Geolocation from './Gelocation';
+import Switch from 'react-switch';
 class DataComp extends React.Component
 {
-  state = {data:[]}
+  state = {checked:false, data:[]}
 
   parentCallBackFunct = (data) =>{
     this.setState({data});
+    console.log(data);
+  }
+  
+  handleChange = (checked) => {
+    this.setState({ checked });
   }
 
   render(){
     return (
       <div>
-        <InputSearch getData={this.parentCallBackFunct}/>
-        <Geolocation getData={this.parentCallBackFunct}/>
+
+        <label>
+          <span>Get Weather for Current Location</span>
+          <Switch onChange={this.handleChange} checked={this.state.checked} className="react-switch" />
+        </label>
+        {this.state.checked === false? 
+        <InputSearch getData={this.parentCallBackFunct}/> : <Geolocation getData={this.parentCallBackFunct}/> }
       </div>
 
     )
   }
 }
+
 
 
 export default DataComp;
