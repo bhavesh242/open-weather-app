47f84658920e5e0c876d4467d834c3aa8aa1e9e1
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-05-16 02:26:58 -0400

    Added Modules for ForeCast and Today Weather, added dependencies for Moment.js

---
+++ src/TodayWeather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class TodayWeather extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                <h2>Today's Weather</h2>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default TodayWeather;
