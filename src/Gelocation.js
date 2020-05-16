4d183d9206545b9b0f4a75600a49da23545ddda3
steve <stevemenezes70@gmail.com>
2020-05-15 22:18:29 -0400

    Geolocation issues fixed

--- src/Gelocation.js
+++ src/Gelocation.js
@@ -4,25 +4,32 @@ class Geolocation extends React.Component{
     state = {
         lat: 0.0,
         long:0.0,
+        data: [],
+        loading: false,
       }
-      componentDidMount(){
+      componentDidMount(){  
         if ("geolocation" in navigator){
           console.log("Available");
           navigator.geolocation.getCurrentPosition((position)=>{
             console.log("Latitiude:",position.coords.latitude);
             console.log("Longitude:",position.coords.longitude);
             this.setState({lat: position.coords.latitude, long: position.coords.longitude});
-            console.log(this.state);
           });
+          this.setState({loading:true});
+          fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+this.state.lat+'&lon='+this.state.long+'&exclude=hourly,daily&appid=69c4928e7eaf7c363ff44bf422b8b7e4')
+          .then(data=> data.json())
+          .then(data=> this.setState({data: data, loading: false})).then(data=> this.props.getData(this.state.data));  
         }
         else{
           console.log("Unavailable");
         }
       }
-    
+
       render(){
+            
         return (
           <div>
+            <h3>{this.state.data? <p>Data: {this.state.data.cnt}</p>:""}</h3> 
             <h3>{this.state.lat? <p>Latitiude: {this.state.lat}</p>:""}</h3>
             <h3>{this.state.long? <p>Longitude: {this.state.long}</p>:""}</h3>
           </div>
