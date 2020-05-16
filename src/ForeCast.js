e70245048934eed3c9ac1b64460769a0ab96d951
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-05-16 07:53:31 -0400

    Forecast Datetime formatting and Modularization

--- src/ForeCast.js
+++ src/ForeCast.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import './index.css';
+var moment = require('moment');
+class ForeCast extends React.Component {
 
-class ForeCast extends React.Component{
-
-    state = {loaded:false};
-    componentDidMount()
-    {
-        this.setState({loaded:true});
+    state = { loaded: false };
+    componentDidMount() {
+        this.setState({ loaded: true });
     }
-    render(){
-        return(
-            <div class="row" style={{textAlign:"center"}}>
-            {this.state.loaded? [0,1,2,3].map(
-                (i)=>{
-                    console.log(this.props)
-                    return(
-                    <div class="col-md-3">
-                    <div className="card">
-                    <h3 className="card-title">{this.props.forecast.datetime[i]}</h3>
-                    <i className={"owf owf-"+this.props.forecast.icon[i]+" owf-5x"}  ></i>
-                    <p className="text-muted">{this.props.forecast.description[i]}</p>
-                    <h2>{this.props.forecast.temp[i]} °F</h2>
-                    <div className="card-body">
-                    <p className="card-text">{this.props.forecast.weather[i]}</p>
-                    </div>
-                    </div>
-                    </div>
+    render() {
+        return (
+            <div class="row" style={{ textAlign: "center" }}>
+                {this.state.loaded ? [0, 1, 2, 3].map(
+                    (i) => {
+
+                        console.log(this.props)
+                        return (
+                            <CardForecast indexNo={i} forecast={this.props.forecast} />
+                        )
+                    }
                 )
+                    : ""
                 }
-            ) : console.log(this.props.forecast)}
             </div>
-            
+
         )
     }
 }
 
+const CardForecast = ({ indexNo, forecast }) => {
+    
+  let newDate = new Date();
+  const weekday = forecast.datetime[indexNo] * 1000
+  newDate.setTime(weekday)
+    
+    return (
+        <div class="col-md-3">
+            <div className="card">
+                 <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+                <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+                <i className={"owf owf-" + forecast.icon[indexNo] + " owf-5x"}  ></i>
+                <p className="text-muted">{forecast.description[indexNo]}</p>
+                <h2>{forecast.temp[indexNo]} °F</h2>
+                <div className="card-body">
+                    <p className="card-text">{forecast.weather[indexNo]}</p>
+                </div>
+            </div>
+        </div>
 
+    )
+}
 
 
 export default ForeCast;
