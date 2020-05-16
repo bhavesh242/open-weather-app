9d6b2d7037dc51710b05c71347c28336ee931d47
steve <stevemenezes70@gmail.com>
2020-05-16 08:01:10 -0400

    today weather live rendering added

--- src/TodayWeather.js
+++ src/TodayWeather.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 
 class TodayWeather extends React.Component {
-
+    
     render() {
-        
+        console.log(this.props.today)
         return (
-            <div>
-                <h2 className="display-1 jumbotron">Today's Weather</h2>
+            <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+            <div className="card">
+                <h3 className="card-title">{this.props.today.datetime}</h3>
+                <i className={"owf owf-"+this.props.today.icon+" owf-5x"}  ></i>
+                <p className="text-muted">{this.props.today.description}</p>
+                <h2>{this.props.today.temp} °F</h2>
+                <div className="card-body">
+                    <p className="card-text">{this.props.today.weather}</p>
+                </div>
+            </div>  
+            </div>  
             </div>
+
         )
     }
 }
