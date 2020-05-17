7569e4260c39f56731aae76bad219ee84a647647
steve <stevemenezes70@gmail.com>
2020-05-17 06:49:32 -0400

    initial commit

---
+++ build/precache-manifest.c1e1c18da31fbf6a66bb3b94ff878c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2753180f3ae5b70aef478606b98e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe9cb7fc95de6306877",
+    "url": "/static/css/main.92ed623b.chunk.css"
+  },
+  {
+    "revision": "c64635d5a48c7944fa7c",
+    "url": "/static/js/2.09cbb2e8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.09cbb2e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe9cb7fc95de6306877",
+    "url": "/static/js/main.f82df023.chunk.js"
+  },
+  {
+    "revision": "e3db8c9e97d10d242f25",
+    "url": "/static/js/runtime-main.4ca1aa5c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
